892e81345b52f02cc571a79aab781a8f590e4fb7
Md Sujan <khans8962@gmail.com>
2022-04-14 11:54:15 +0530

    added connected socket drawing

--- public/canvas.js
+++ public/canvas.js
@@ -1,6 +1,8 @@
 // when window loads
 window.addEventListener('load', () => {
     socket = io.connect('http://localhost:3000')
+    socket.on('mouseData', newDrawing);
+    
     function initCanvas(canvas){
         canvas.height = window.innerHeight-100;
         canvas.width = window.innerWidth-250;
@@ -17,7 +19,16 @@ window.addEventListener('load', () => {
     })
     var ctx = canvas.getContext("2d"); // for drawing
 
-
+    function newDrawing(data){
+        console.log("Client received:"+data.x+" "+data.y);
+        ctx.lineWidth = 5;
+        ctx.lineCap = "round"
+        ctx.strokeStyle = 'blue';
+        ctx.lineTo(data.x,data.y);
+        ctx.moveTo(data.x,data.y);
+        ctx.stroke();
+            
+    }
     // freehand drawing
     let freehand = false;
     function startFree(e){
@@ -39,10 +50,14 @@ window.addEventListener('load', () => {
             ctx.lineTo(e.clientX,e.clientY);
             // ctx.beginPath();
             ctx.moveTo(e.clientX,e.clientY);
+            var data = {
+                x: e.clientX,
+                y: e.clientY
+            };
+            socket.emit('mouseData', data);
             ctx.stroke();
         }
         return
-        
     }
     
     canvas.addEventListener('mousedown',startFree);
--- server.js
+++ server.js
@@ -18,4 +18,11 @@ io.sockets.on('connection', newConnection);
 
 function newConnection(socket){
     console.log("A new user connectd: new user socket id:\t"+socket.id);
+    socket.on('mouseData', mouseMessage);
+
+    function mouseMessage(data){
+        console.log("Server received:"+data.x+" "+data.y);
+        socket.broadcast.emit('mouseData', data);
+
+    }
 }
