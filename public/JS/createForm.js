ab11616c39af65f6dd97f94aa5b94b5535def940
Md Sujan <khans8962@gmail.com>
2022-04-27 11:33:41 +0530

    added login via azure using ms account

---
+++ public/JS/createForm.js
@@ -0,0 +1,10 @@
+var myForm = document.getElementById('form_create');
+const url = "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize?client_id=71c8ebe1-c1eb-4200-9645-0a16441defe8&redirect_uri=http://localhost:3000/canvas.html&scope=user.read&response_type=code&response_mode=query";
+myForm.onsubmit = (e) => {
+    e.preventDefault();
+    if(! /\?code=[\w+\.\-]{45}/g.test(window.location.search)){
+        window.location.href=url;
+    }else{
+        window.location.href="/canvas.html";
+    }
+}
