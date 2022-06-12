a6117e1fd68ff1ac3d31311443bf722e4ad0db31
Md Sujan <khans8962@gmail.com>
2022-06-12 13:31:11 +0530

    sso uri fix

--- public/JS/createForm.js
+++ public/JS/createForm.js
@@ -1,6 +1,6 @@
 var myForm = document.getElementById("form_create");
 const url =
-  "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize?client_id=71c8ebe1-c1eb-4200-9645-0a16441defe8&redirect_uri=https://online-whiteboard-2022-cse-38.herokuapp.com/canvas.html&scope=user.read&response_type=code&response_mode=query";
+  "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize?client_id=71c8ebe1-c1eb-4200-9645-0a16441defe8&redirect_uri=https://online-whiteboard-2022-cse-38.herokuapp.com//canvas.html&scope=user.read&response_type=code&response_mode=query";
 myForm.onsubmit = (e) => {
   e.preventDefault();
   const roomData = {};
