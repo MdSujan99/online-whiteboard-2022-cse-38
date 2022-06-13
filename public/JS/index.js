a078831f93d12ab36f3ea85aba96957e1f4e7a54
Md Sujan <khans8962@gmail.com>
2022-06-13 14:11:58 +0530

    login bypass test

--- public/JS/index.js
+++ public/JS/index.js
@@ -17,6 +17,6 @@ myForm.onsubmit = (e) => {
     console.log("if true");
   } else {
     console.log("else false");
-    // window.location.href = "/index.html";
+    window.location.href = "/create.html";
   }
 };
